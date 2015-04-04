62b1d4a48d1425d0dd7eb9e1e5d45c7ca13f46f9
Andrew Koropatva <akoropatva@chegg.com>
2015-04-04 15:11:11 +0300

    #4 Modified Person to Station model

--- bus/src/main/java/com/bus/service/dao/models/Station.java
+++ bus/src/main/java/com/bus/service/dao/models/Station.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.joda.time.DateTime;
+
 @Entity
 @Table(name = "Station")
 public class Station {
@@ -16,9 +18,15 @@ public class Station {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	private int busId;
+
 	private String name;
 
-	private String country;
+	private String phone;
+
+	private String address;
+
+	private DateTime lastBusModification;
 
 	public int getId() {
 		return id;
@@ -36,16 +44,43 @@ public class Station {
 		this.name = name;
 	}
 
-	public String getCountry() {
-		return country;
+	public int getBusId() {
+		return busId;
 	}
 
-	public void setCountry(String country) {
-		this.country = country;
+	public void setBusId(int busId) {
+		this.busId = busId;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public DateTime getLastBusModification() {
+		return lastBusModification;
+	}
+
+	public void setLastBusModification(DateTime lastBusModification) {
+		this.lastBusModification = lastBusModification;
 	}
 
 	@Override
 	public String toString() {
-		return "id=" + id + ", name=" + name + ", country=" + country;
+		return "Station [id=" + id + ", busId=" + busId + ", name=" + name
+				+ ", phone=" + phone + ", address=" + address
+				+ ", lastBusModification=" + lastBusModification + "]";
 	}
+
 }
